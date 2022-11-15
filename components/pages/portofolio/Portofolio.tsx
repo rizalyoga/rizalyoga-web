3bc9d616a4dea23d67743c077937a529cba42b04
rizalyoga <ach.rizal.yogaswara@gmail.com>
2022-11-15 12:52:16 +0700

    remove image on portofolio

--- components/pages/portofolio/Portofolio.tsx
+++ components/pages/portofolio/Portofolio.tsx
@@ -1,58 +1,63 @@
-import React, {useState, useEffect} from 'react'
-import Layout from '../../../layout/Layout'
-import styles from "./Portofolio.module.scss"
+import React, { useState, useEffect } from 'react';
+import Layout from '../../../layout/Layout';
+import styles from './Portofolio.module.scss';
 
 // Component
-import Loader from 'react-loaders'
-import AnimateLetters from '../../animations/animatedLetters/AnimateLetters'
-import Card from "../../cards/Card"
+import Loader from 'react-loaders';
+import AnimateLetters from '../../animations/animatedLetters/AnimateLetters';
+import Card from '../../cards/Card';
 
 const PortofolioPage = () => {
   const [letterClass, setLetterClass] = useState<string>('text-animate');
-  const nameArray = ['M','y',' ','P','r','o','j','e','c','t'];
+  const nameArray = ['M', 'y', ' ', 'P', 'r', 'o', 'j', 'e', 'c', 't'];
 
-  useEffect(():any => {
+  useEffect((): any => {
     setTimeout(() => {
-      setLetterClass('text-animate-hover')
-    }, 4000)
+      setLetterClass('text-animate-hover');
+    }, 4000);
   }, []);
 
   return (
-    <Layout title=' | Projects'>
+    <Layout title=" | Projects">
       <div className={`container ${styles['portofolio-page']}`}>
-        <div className={styles["text-zone"]}>
+        <div className={styles['text-zone']}>
           <h1>
-          <AnimateLetters 
-            letterClass={letterClass} 
-            strArray={nameArray}
-            idx={15}
-          />
+            <AnimateLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
 
-          <div className={styles.desc}>
-            <p style={{'animation': "pulse 2s"}}>
-              Hello 👋, These are some of the projects I completed in the past.
-            </p>
-            <p style={{'animation': "pulse 2s"}}>
-              Most of the technologies that I use in my project work are  
-              <span> HTML, CSS3, SASS, Javascript, Bootstrap, Tailwind CSS, ReactJS, 
-              NextJS, axios </span> for consume API data, and <span> Github </span> 
-              to collaborate with team members. 
-            </p>
-          </div>
-          
-          <br />
+            <div className={styles.desc}>
+              <p style={{ animation: 'pulse 2s' }}>
+                Hello 👋, These are some of the projects I completed in the
+                past.
+              </p>
+              <p style={{ animation: 'pulse 2s' }}>
+                Most of the technologies that I use in my project work are
+                <span>
+                  HTML, CSS3, SASS, Javascript, Bootstrap, Tailwind CSS,
+                  ReactJS, NextJS, axios
+                </span>
+                for consume API data, and <span> Github </span>
+                to collaborate with team members.
+              </p>
+            </div>
+
+            <br />
           </h1>
         </div>
+
         <div className={styles['content-zone']}>
-            <div className={styles['card-container']}>
-                <Card/>
-            </div>
+          <div className={styles['card-container']}>
+            <Card />
+          </div>
         </div>
       </div>
-      
-      <Loader type='ball-scale-multiple' active/>
-    </Layout>
-  )
-}
 
-export default PortofolioPage
+      <Loader type="ball-scale-multiple" active />
+    </Layout>
+  );
+};
+
+export default PortofolioPage;
